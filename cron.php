bc354450ce2658c7d567b4729fe937b69a3fac67
Ricardo Moraleida <moraleida@gmail.com>
2014-05-02 17:41:50 -0300

    cron

--- cron.php
+++ cron.php
@@ -1,6 +1,6 @@
 <?php 
+ini_set('error_reporting', '1');
 
-// wp_die(var_dump($_GET));
 /* API Pagseguro */
 if($_GET['pag']) {
 	reserva_wp_cron_check_pagamentos();
@@ -8,7 +8,7 @@ if($_GET['pag']) {
 
 add_action( 'reserva_wp_cron_daily_hook', 'reserva_wp_cron_check_expires' );
 add_action( 'reserva_wp_cron_daily_hook', 'reserva_wp_cron_check_removes' );
-// add_action( 'reserva_wp_cron_daily_hook', 'reserva_wp_cron_check_pagamentos' );
+add_action( 'reserva_wp_cron_daily_hook', 'reserva_wp_cron_check_pagamentos' );
 
 // Roda uma vez na ativação, agendando o cron
 function reserva_wp_cron_job_schedule() {
@@ -84,8 +84,8 @@ function reserva_wp_cron_check_pagamentos() {
 	$DB_TYPE="mysql";
 	$retorno_host = 'localhost'; // Local da base de dados MySql
 	$retorno_database = 'ecotempo_main'; // Nome da base de dados MySql
-	$retorno_usuario = 'ecotempo_admin'; // Usuario com acesso a base de dados MySql 
-	$retorno_senha = 'ZpaK}GN5ni({';  // Senha de acesso a base de dados MySql
+	$retorno_usuario = 'ecotempo'; // Usuario com acesso a base de dados MySql 
+	$retorno_senha = 'J3eWlq929b';  // Senha de acesso a base de dados MySql
 
 	$mysqli = new mysqli($retorno_host, $retorno_usuario, $retorno_senha, $retorno_database);
 	if($mysqli->connect_errno)
@@ -94,7 +94,7 @@ function reserva_wp_cron_check_pagamentos() {
 	/* Definindo as credenciais  */    
 	$credentials = new PagSeguroAccountCredentials(      
 	    'andre@eaxdesign.com.br',       
-	    '60EE5486C8B444A7BA219F7F9D82414A'      
+	    '8B15EE8E21634441882BADC042B182C9'
 	);  
 
 	$transactions = get_posts( array( 'post_type' => 'rwp_transaction', 
@@ -111,44 +111,54 @@ function reserva_wp_cron_check_pagamentos() {
 
 	foreach ($transactions as $t) {
 		$obj = get_post_meta($t->ID, 'rwp_transaction_object', true);
-		$in[] = $obj.'::'.$t->ID;
+		$in[] = $obj.'-'.$t->ID;
 	}
 
 	$in = join(',', $in);
 
-	$res = $mysqli->query("SELECT TransacaoID,Rererencia FROM PagSeguroTransacoes WHERE Referencia IN ($in)");
+	$sql = "SELECT TransacaoID,Referencia FROM `PagSeguroTransacoes` WHERE Referencia IN (".$in.")";
+	$res = $mysqli->query($sql);
 
-	while($row = $res->fetch_assoc()) {
-		$tids[$row['Referencia']] = $row['TransacaoID'];
+	/* Códigos identificadores da transação  */
+	if($res) {
+		while($row = $res->fetch_assoc()) {
+			$tids[$row['Referencia']] = $row['TransacaoID'];
+		}		
 	}
 
-	/* Código identificador da transação  */    
-	// $transaction_id = 'BA83420F-F723-4270-AEB4-1D08B188A798';  
+	
+	$statuses = array();
 
+	if($tids) :
 	foreach($tids as $key => $value) {
 		/*  
 		    Realizando uma consulta de transação a partir do código identificador  
 		    para obter o objeto PagSeguroTransaction 
 		*/   
-		$transaction = PagSeguroTransactionSearchService::searchByCode( $credentials, $value );  
-		$status = $transaction->getStatus();  
-		$intTrans = explode('::', $key);
+		try {
+			$transaction = PagSeguroTransactionSearchService::searchByCode( $credentials, $value );  
+			$status = $transaction->getStatus();  
+			$intTrans = explode('-', $key);
 
-		if($status < 3) { // 1 == aguardando / 2 == análise
-			// update_post_meta( $intTrans[1], 'rwp_transaction_status', 'aguardando' );
-		}
-		if(3 == $status || 4 == $status) { // 3 == pago / 4 == disponivel
-			// update_post_meta( $intTrans[1], 'rwp_transaction_status', 'liberado' );
-		}
-		if($status > 4) { // 5 == disputa / 6 == devolvida / 7 == cancelada
-			// update_post_meta( $intTrans[1], 'rwp_transaction_status', 'retirado' );
-		}
+			if($status < 3) { // 1 == aguardando / 2 == análise
+				update_post_meta( $intTrans[1], 'rwp_transaction_status', 'aguardando' );
+			}
+			if(3 == $status || 4 == $status) { // 3 == pago / 4 == disponivel
+				update_post_meta( $intTrans[1], 'rwp_transaction_status', 'liberado' );
+			}
+			if($status > 4) { // 5 == disputa / 6 == devolvida / 7 == cancelada
+				update_post_meta( $intTrans[1], 'rwp_transaction_status', 'retirado' );
+			}
 
+		} catch (PagSeguroServiceException $e) {
+			// echo '<pre>'; var_dump($e); echo '</pre>';
+            $status = $e->getMessage();
+        }
+		
 		$statuses[$intTrans[1]] = $status;
 	}
+	endif;
 
-	if($_GET['pag'])
-		var_dump($statuses);
 }
 
 ?>
